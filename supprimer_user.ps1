429ba37585e203f8aba85b9865c61828da4bb20d
Noe-Moreau <110332@ecole-it.com>
2026-06-10 10:10:40 +0200

    merge ds branch

--- supprimer_user.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "=== ATTENTION SUPPRESSION D'UN UTILISATEUR ==="
-$Prenom = Read-Host "Entrez votre prenom"
-$Nom = Read-Host "Entrez votre nom"
-
-$name = ($Prenom[0] + "." + $Nom).ToLower()
-
-Remove-ADUser -Identity $name -Confirm:$false
-Write-Host "Utilisateur $name supprime."
